04136b495c0bcba25422229c77d7018d8da1149e
beamguide <beamguide@gmail.com>
2019-02-08 22:41:17 +0700

    update

--- prebuilt/app/Message/Android.mk
+++ prebuilt/app/Message/Android.mk
@@ -19,7 +19,7 @@ LOCAL_MODULE := Message
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_BUILT_MODULE_STEM := package.apk
-LOCAL_OVERRIDES_PACKAGES := Messaging
+LOCAL_OVERRIDES_PACKAGES := messaging
 # Make sure the build system doesn't try to resign the APK
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_DEX_PREOPT := false
